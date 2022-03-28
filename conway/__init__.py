18acab751aa4116cacf5e7fdfc4b0ce013273a5a
Bert Tijskens <engelbert.tijskens@uantwerpen.be>
2022-03-28 15:44:28 +0200

    naive version coded and tested

--- conway/__init__.py
+++ conway/__init__.py
@@ -1,22 +1,192 @@
 # -*- coding: utf-8 -*-
 
 """
-Package conway
+Package conway_mac
 =======================================
 
-Top-level package for conway.
+Top-level package for conway_mac.
+
+Conway's game of life
+*********************
+
+Some useful/interesting links
+
+* `Game of life <https://nl.wikipedia.org/wiki/Game_of_Life>`_
+
+
+Concepts
+--------
+* infinite grid of square cells
+* cells are either dead or alive
+* each cell has 8 neighbours. Let ``A_ij`` be the number of alive neighbours of cell ``ij``.
+  A populated cells remains populated, only if 2 or 3 or its neighbours are populated.
+  Otherwise, the cell's population dies (either of loneliness or overpopulation).
+  A non-populated cell is populated, if it has exactly 3 populated neighbouring cells
+  Note that one must count the neighbours of **all** cells before updating states.
+
+Implementation ideas
+--------------------
+* We cannot store an infinite grid, as memory is finite.
+* We could store only the living cells and their location. That would allow for a finite
+  number of cells on an infinite grid. That would be a rather complex idea to begin with
+* We could start with a finite grid and use a reflecting boundary, surrounding the grid
+  with an extra layer which has the same state as the neighbouring layer on the inside.
+  e.g. on a 4x4 grid::
+
+    0 | 0 0 1 0 | 0
+    --+---------+--
+    0 | 0 0 1 0 | 0
+    0 | 0 1 0 0 | 0
+    0 | 0 0 1 0 | 0
+    0 | 0 1 0 1 | 1
+    --+---------+--
+    0 | 0 1 0 1 | 1
+
+  I.e., we would store a 6x6 grid with a different rule to update the outer cells.
+
+* We could apply periodic boundary conditions, i.e. the surrounding layer would be a copy
+  of the layer at the other end::
+
+    1 | 0 1 0 1 | 0
+    --+---------+--
+    0 | 0 0 1 0 | 0
+    0 | 0 1 0 0 | 0
+    0 | 0 0 1 0 | 0
+    1 | 0 1 0 1 | 0
+    --+---------+--
+    0 | 0 0 1 0 | 0
+
+  That would be some sort of infinity.
+
+
+Let's start off with a finite grid.
+
 """
 
 __version__ = "0.0.0"
 
+import numpy as np
 
-def hello(who='world'):
-    """'Hello world' method.
 
-    :param str who: whom to say hello to
-    :returns: a string
+class FiniteGrid:
+    """Naive NxN grid with.
+
+    In order to not have to implement special boundary rules, we make the grid
+    (N+2)x(N+2) where the outer layers are always zero::
+
+        0 | 0 0 0 0 | 0
+        --+---------+--
+        0 | 0 0 1 0 | 0
+        0 | 0 1 0 0 | 0
+        0 | 0 0 1 0 | 0
+        0 | 0 1 0 1 | 0
+        --+---------+--
+        0 | 0 0 0 0 | 0
+
     """
-    result = "Hello " + who
-    return result
+    boundary_conditions = ('zero', 'reflect', 'periodic')
 
-# Your code here...
+    def __init__(self, N=10, boundary='zero'):
+        self.N = N
+        self.n = N + 2
+        rng = np.random.default_rng()
+        self.states = rng.integers(0, high=2, size=(N+2,N+2))
+        # correct the boundaries
+        if 'zero'.startswith(boundary):
+            self.bc = 'zero'
+            # boundaries are zeros
+            self.apply_0bc()
+
+        elif 'reflect'.startswith(boundary):
+            self.bc = 'reflect'
+            self.apply_rbv()
+
+        elif 'periodic'.startswith(boundary):
+            self.bc = 'periodic'
+            self.apply_pbc()
+
+        else:
+            raise ValueError("boundary not in ('zero', 'reflect', 'periodic')")
+
+        self.counts = np.zeros_like(self.states, dtype=int)
+
+    def apply_bc(self, bc=None):
+        if not bc is None:
+            self.bc = bc
+        if self.bc == 'zero':
+            self.apply_0bc()
+        elif self.bc == 'reflect':
+            self.apply_rbc()
+        elif self.bc == 'periodic':
+            self.apply_pbc()
+        else:
+            raise ValueError("boundary not in ('zero', 'reflect', 'periodic')")
+
+    def apply_0bc(self):
+        N = self.N
+        self.states[:    , 0    ] = 0
+        self.states[:    , N + 1] = 0
+        self.states[0    , 1:N+1] = 0
+        self.states[N + 1, 1:N+1] = 0
+
+    def apply_rbc(self):
+        """Apply reflecting boundary condition."""
+        N = self.N
+        self.states[:, 0    ] = self.states[:, 1]
+        self.states[:, N + 1] = self.states[:, N]
+        self.states[0, :    ] = self.states[1, :]
+        self.states[N + 1, :] = self.states[N, :]
+
+    def apply_pbc(self):
+        N = self.N
+        # edges
+        self.states[1:N + 1, 0] = self.states[1:N + 1, N]
+        self.states[1:N + 1, N + 1] = self.states[1:N + 1, 1]
+        self.states[0, 1:N + 1] = self.states[N, 1:N + 1]
+        self.states[N + 1, 1:N + 1] = self.states[1, 1:N + 1]
+        # corners
+        self.states[0    , 0    ] = self.states[N, N]
+        self.states[N + 1, N + 1] = self.states[1, 1]
+        self.states[0    , N + 1] = self.states[N, 1]
+        self.states[N + 1, 0    ] = self.states[1, N]
+
+    def print(self):
+        if self.bc == 'zero':
+            for i in range(1,self.n-1):
+                s = ''
+                for j in range(1,self.n-1):
+                    s += f"{self.states[i, j]} "
+                print(s)
+        else:
+            print(f'BC = {self.bc}')
+            for i in range(self.n):
+                s = ''
+                for j in range(self.n):
+                    s += f"{self.states[i, j]} "
+                print(s)
+        print()
+
+    def evolve(self):
+        """loop over all cells and count its alive neighbours."""
+        N = self.N
+
+        for i in range(1,N+1):
+            for j in range(1,N+1):
+                self.counts[i, j] = self.states[i-1, j-1] \
+                                  + self.states[i-1, j  ] \
+                                  + self.states[i-1, j+1] \
+                                  + self.states[i  , j-1] \
+                                  + self.states[i  , j+1] \
+                                  + self.states[i+1, j-1] \
+                                  + self.states[i+1, j  ] \
+                                  + self.states[i+1, j+1]
+        for i in range(1,N+1):
+            for j in range(1,N+1):
+                if self.states[i,j] == True: # populated
+                    if not self.counts[i,j] in (2,3): # cell population dies
+                        self.states[i,j] = False
+                else:
+                    if self.counts[i,j] == 3:
+                        self.states[i,j] = True
+
+        self.apply_bc()
--- tests/conway/test_conway.py
+++ tests/conway/test_conway.py
@@ -2,19 +2,86 @@
 
 """Tests for conway package."""
 
-import conway
+import sys
+
+import numpy as np
+
+sys.path.insert(0,'.')
+
+import conway as cnw
 
 
-def test_hello_noargs():
-    """Test for conway.hello()."""
-    s = conway.hello()
-    assert s == "Hello world"
+def test_FiniteGrid():
+    fg = cnw.FiniteGrid()
+    for bc in cnw.FiniteGrid.boundary_conditions:
+        fg.apply_bc(bc)
+        fg.print()
+        N = fg.N
+        s = fg.states
+        for i in range(N + 2):
+            for j in range(N + 2):
+                if i in (0 ,N+1) and j in (0, N+1): # a boundary cell
+                    if bc == 'zero':
+                        assert s[i,j] == 0
+                    else:
+                        if bc == 'reflect':
+                            i0 = 1 if (i == 0) else N
+                            j0 = 1 if (j == 0) else N
+                        else:  # 'periodic'
+                            i0 = N if (i == 0) else 1
+                            j0 = N if (j == 0) else 1
+                        assert s[i,j] == s[i0,j0]
+                else:
+                    assert fg.states[i,j] in (0,1)
 
+def test_evolve():
+    for i in range(10000):
+        print(i)
+        fg = cnw.FiniteGrid(3)
+        fg.print()
+        center_alive = fg.states[2,2]
+        center_count = np.sum(fg.states[1:4,1:4]) - fg.states[2,2]
+        if center_alive:
+            expected = center_count in (2,3)
+        else:
+            expected = center_count == 3
+        print(center_alive, center_count, expected, "=======")
+        fg.evolve()
+        fg.print()
+        assert fg.states[2,2] == expected
 
-def test_hello_me():
-    """Test for conway.hello('me')."""
-    s = conway.hello('me')
-    assert s == "Hello me"
+def test_evolve1():
+    fg = cnw.FiniteGrid(3)
+    fg.states[:,:] = 0
+    fg.states[1:4,1] = 1
+    fg.print()
+    center_alive = fg.states[2,2]
+    center_count = np.sum(fg.states[1:4,1:4]) - fg.states[2,2]
+    if center_alive:
+        expected = center_count in (2,3)
+    else:
+        expected = center_count == 3
+    print(center_alive, center_count, expected, "=======")
+    fg.evolve()
+    fg.print()
+    assert fg.states[2,2] == expected
+
+def test_evolve2():
+    fg = cnw.FiniteGrid(3)
+    fg.states[:,:] = 0
+    fg.states[1:4,1:4] = np.array([[0,1,1],[0,1,0],[0,0,1]])
+    fg.print()
+    center_alive = fg.states[2,2]
+    center_count = np.sum(fg.states[1:4,1:4]) - fg.states[2,2]
+    if center_alive:
+        expected = center_count in (2,3)
+    else:
+        expected = center_count == 3
+    print(center_alive, center_count, expected, "=======")
+    fg.evolve()
+    fg.print()
+    assert fg.states[2,2] == expected
+
 
 
 # ==============================================================================
@@ -24,7 +91,7 @@ def test_hello_me():
 # that the source directory is on the path
 # ==============================================================================
 if __name__ == "__main__":
-    the_test_you_want_to_debug = test_hello_noargs
+    the_test_you_want_to_debug = test_evolve
 
     print("__main__ running", the_test_you_want_to_debug)
     the_test_you_want_to_debug()
